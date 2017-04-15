c193ecaace7ec18c497d54896333e7288ce08eff
luxsafari <luxsafari@hotmail.com>
2017-04-15 18:06:56 +0900

    3rd

---
+++ api.py
@@ -0,0 +1,18 @@
+# api.py
+
+import falcon
+import json
+
+class HogeResource(object):
+    def on_get(self, req, resp, id):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({ 
+                      "result": true, 
+                      "message": "success!",
+                    })
+
+api = falcon.API()
+hoge = HogeResource()
+
+api.add_route('/hoge/{id}', hoge)
